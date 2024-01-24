700cc42e20f76b535a256ea76616220ed6923f1a
jonathan hamilton <longjonns@gmail.com>
2024-01-24 13:25:09 +0000

    working example

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,32 @@
 import Vapor
 
+struct FormContent: Content {
+    let name: String
+}
+
 func routes(_ app: Application) throws {
     app.get { req async throws in
         try await req.view.render("index", ["title": "Hello Vapor!"])
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.post("form") { req async -> Response in
+
+        do {
+            let formData = try req.content.decode(FormContent.self)
+
+            let name = formData.name
+            
+            let html_response = """
+                <h2>\(name)</h2>
+            """
+
+            var response = Response(status: .ok)
+            response.headers.contentType = .html
+            response.body = .init(string: html_response)
+
+            return response
+        } catch {
+            return Response(status: .badRequest)
+        } 
     }
 }
